8e1cc88bf75575180f49e3e862f645f2acf51817
hayksaakian <fakest.id.ever@gmail.com>
2013-01-17 16:44:50 -0800

    more lorem ipsum

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-var lawnchair = Lawnchair({name:'lawnchair'},function(e){
+var lawnchair = Lawnchair({name:'test_db'},function(e){
   console.log('storage open');
   count();
 });
 var lorem = '';
+var counter = 0;
 $(document).on('click', 'button', function(){
 	var id = $(this).attr('id');
 	lorem = $('#lorem').text();
@@ -14,6 +15,8 @@ $(document).on('click', 'button', function(){
 		add_dummies(9001);
 	}else if(id == 'read_all'){
 		read_all();
+	}else if(id == 'count'){
+		count();
 	}else if(id == 'nuke'){
 		nuke();
 		$('#counter').text('0');
@@ -27,31 +30,79 @@ function add_dummies(quantity){
 		lawnchair.save({key:uuid, value:{num:val, text:lorem}})
 	};
 	$ct = $('#counter')
-
+	counter += quantity;
 	$ct.text(parseInt($ct.text())+quantity)
 }
 
 function nuke(){
+	counter = 0;
 	lawnchair.nuke();
 }
 
 function read_all(){
+	console.log('starting read all');
 	var output = [];
-	var total = parseInt($('#counter').text());
+	var total = counter;
   lawnchair.each(function(dummy, i){
   	//console.log(dummy.value);
-  	//output.push(dummy.value);
+  	output.push(dummy.value);
   	if (total - 1 == i){
-  		console.log('done');
+  		console.log('done reading '+i+' records');
 		  //$('#output').html(output.join('<br>'));  		
+  	}else {
+  		if (total % i <= 3){
+	  		console.log(i+'...'+dummy.key)
+  		}
   	}
   });
 }
-var counter = 0;
 function count(){
+	console.log('counting');
   $('#counter').text(0);  
-	lawnchair.each(function(derp, i){
-		counter = i+1;
+	lawnchair.all(function(derp){
+		counter = derp.length;
 	  $('#counter').text(counter);  		
 	});
+}
+
+// fffb22eb-79e4-5f00-0ada-302d9b66bf43 
+// getSizeOfPersisted('fffb22eb-79e4-5f00-0ada-302d9b66bf43')
+
+function getSizeOfPersisted(key){
+	lawnchair.get(key, function(obj){
+  	console.log(roughSizeOfObject(obj));
+  });
+}
+
+function roughSizeOfObject( object ) {
+  var objectList = [];
+  var stack = [ object ];
+  var bytes = 0;
+
+  while ( stack.length ) {
+      var value = stack.pop();
+
+      if ( typeof value === 'boolean' ) {
+          bytes += 4;
+      }
+      else if ( typeof value === 'string' ) {
+          bytes += value.length * 2;
+      }
+      else if ( typeof value === 'number' ) {
+          bytes += 8;
+      }
+      else if
+      (
+          typeof value === 'object'
+          && objectList.indexOf( value ) === -1
+      )
+      {
+          objectList.push( value );
+
+          for( i in value ) {
+              stack.push( value[ i ] );
+          }
+      }
+  }
+  return bytes;
 }
